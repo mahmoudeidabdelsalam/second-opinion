d213cc8794ac85d2fa71cb969925f8a007452512
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-12-07 22:53:54 +0200

    build(vuetify): :heavy_plus_sign: add vuetify2

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
-
 import App from "./App.vue";
+
+// router
 import router from "./router";
+
+// vuetify
+import vuetify from "./plugins/vuetify";
 
 import "./assets/style/main.scss";
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#585ce5",
+        primary_bg: "#ebecfc",
+        black: "#1c295d",
+        secondary: "#697591",
+        secondary_bg: "#ebecfc",
+        error: "#FA6767",
+        error_bg: "#f9ebeb",
+        info: "#54c1fb",
+        info_bg: "#eaf7ff",
+        success: "#2bad77",
+        success_bg: "#e9f7f2",
+        warning: "#f97f26",
+        warning_bg: "#fef6eb",
+      },
+    },
+  },
+});
