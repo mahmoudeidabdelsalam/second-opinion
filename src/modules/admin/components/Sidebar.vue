04bf3577856dfe1c423fbee54e69fa2b53d0ad46
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-12-08 21:51:29 +0200

    feat: :sparkles: add admin module with admin main layout includes header and sidebar

---
+++ src/modules/admin/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside class="sidebar white">
+    <router-link
+      :to="{ name: 'Home' }"
+      class="brand d-flex justify-center align-center pa-5 font-weight-bold primary--text text-h5 text-center"
+    >
+      <img src="@/assets/images/logo.webp" alt="Vite Land logo" />
+      <span class="mx-2">Second opinion</span>
+    </router-link>
+
+    <span class="d-block pa-5 font-weight-bold black--text">Menu</span>
+
+    <v-list>
+      <v-list-item link to="#" active-class="primary--text">
+        <v-list-item-icon>
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/sidebar.scss";
+</style>
---
+++ src/modules/admin/views/AdminDashboard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app class="admin-dashboard secondary_bg">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="secondary_bg"
+      :right="systemDirection === 'rtl'"
+    >
+      <!-- sidebar -->
+      <sidebar />
+    </v-navigation-drawer>
+
+    <v-app-bar app color="secondary_bg" elevation="2" elevate-on-scroll>
+      <v-btn icon @click="drawer = !drawer" color="black" aria-label="drawer">
+        <v-icon> mdi-apps </v-icon>
+      </v-btn>
+
+      <v-spacer />
+
+      <admin-header />
+    </v-app-bar>
+
+    <v-main class="pa-4">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import mapGetters from vuex
+import { mapGetters } from "vuex";
+export default {
+  name: "AdminDashboard",
+  components: {
+    AdminHeader: () => import("../components/Header.vue"),
+    Sidebar: () => import("../components/Sidebar.vue"),
+  },
+  data: () => ({ drawer: null }),
+  computed: {
+    // map getters
+    ...mapGetters({
+      systemDirection: "translations/systemDirection",
+    }),
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../style/admin-dashboard.scss";
+</style>
