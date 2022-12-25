d001282855f958eb85ab42aaae2985cf1d1fdc54
Mohamed Omar <femohamedomar@globaldirections.net>
2022-12-25 16:02:23 +0200

    feat(client): :sparkles: add client notifications page

--- src/modules/client/views/Notifications.vue
+++ src/modules/client/views/Notifications.vue
@@ -1,11 +1,34 @@
 <template>
-  <main class="pa-10 text-center">
-    <h1 class="mb-5">Notifications page</h1>
+  <main class="notifications pa-5">
+    <span class="d-block font-weight-bold mb-5 primary--text text-h5">
+      Notifications
+    </span>
+
+    <div class="content pa-5 elevation-3 rounded-lg">
+      <div
+        class="notification pa-5 rounded-lg secondary_bg mb-5"
+        v-for="notification in 5"
+        :key="notification"
+      >
+        <span class="d-block font-weight-bold mb-2 primary--text text-h6">
+          You have an appointment in 15 minutes
+        </span>
+        <p class="secondary--text mb-5">
+          Reminder of the consultation appointment on Saturday, November 12,
+          2022, stay for 15 minutes
+        </p>
+        <span class="secondary--text body-2">November 6, 2021, 10:43 am</span>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 export default {
-  name: "Notifications",
+  name: "notifications.scss",
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../style/notifications.scss";
+</style>
