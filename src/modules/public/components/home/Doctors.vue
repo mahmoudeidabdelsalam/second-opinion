9bed19f77bac43c8568371c8e785ec2184ce6a69
Mohamed Omar <femohamedomar@globaldirections.net>
2023-01-01 11:40:28 +0200

    feat(home): :sparkles: add doctors section to home page

---
+++ src/modules/public/components/home/Doctors.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="mb-16">
+    <v-container>
+      <h1 class="primary--text text-center mb-10">Doctors</h1>
+
+      <carousel
+        :nav="false"
+        :autoplay="true"
+        :dots="false"
+        :responsive="{
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 2,
+          },
+          1400: {
+            items: 3,
+          },
+        }"
+        style="max-width: 1400px"
+        class="mx-auto"
+      >
+        <div
+          class="pa-5 ma-5 rounded-lg elevation-3"
+          v-for="item in 5"
+          :key="item"
+        >
+          <img
+            src="https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg"
+            class="rounded-lg mb-5"
+          />
+          <h1 class="text-center primary--text mb-2">
+            Dr. Khaled Akram Mohammed
+          </h1>
+          <span
+            class="d-block text-center font-weight-bold text--secondary mb-3"
+          >
+            Specialty/ Internal Medicine
+          </span>
+          <v-btn class="primary rounded-lg mb-2 text-capitalize" block>
+            <v-icon>mdi-video</v-icon>
+            <span class="px-2">Audio/Video Consultation</span>
+          </v-btn>
+          <v-btn class="primary rounded-lg mb-2 text-capitalize" block>
+            <v-icon>mdi-file-multiple</v-icon>
+            <span class="px-2">Request a Medical Report</span>
+          </v-btn>
+          <v-btn class="primary rounded-lg text-capitalize" block>
+            <v-icon>mdi-doctor</v-icon>
+            <span class="px-2">View Doctor's Page</span>
+          </v-btn>
+        </div>
+      </carousel>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "Doctors",
+
+  components: { carousel },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/modules/public/views/Home.vue
+++ src/modules/public/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <main class="home">
     <h1 class="mb-5">Home page</h1>
-    <v-btn class="primary" link :to="{ name: 'Login' }">Login</v-btn>
+    <v-btn class="primary mb-16" link :to="{ name: 'Login' }">Login</v-btn>
+    <doctors />
     <mobile-apps />
     <blog />
     <contact-form />
@@ -13,6 +14,7 @@ export default {
   name: "Home",
 
   components: {
+    Doctors: () => import("../components/home/Doctors.vue"),
     MobileApps: () => import("../components/home/MobileApps.vue"),
     Blog: () => import("../components/home/Blog.vue"),
     ContactForm: () => import("../components/home/ContactForm.vue"),
